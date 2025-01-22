39d02b06542e8818abc6b8a672472396836d04e2
Gabin8K <gabindjomo21@gmail.com>
2025-01-21 21:20:21 -0500

    useLocale auto update language

--- hooks/useLocale.ts
+++ hooks/useLocale.ts
@@ -1,8 +1,11 @@
-import i18n, { t } from "@/locale/i18n";
+import i18n from "@/locale/i18n";
 import { useSetting } from "./useSetting";
 
 export function useLocale() {
   const { locale, setLocale } = useSetting();
+  
+  i18n.locale = locale;
+  const t = i18n.t.bind(i18n);
 
   const changeLocale = (locale: string) => {
     i18n.locale = locale;
--- locale/i18n.ts
+++ locale/i18n.ts
@@ -1,7 +1,7 @@
-import * as Localization from 'expo-localization';
-import { I18n } from 'i18n-js';
 import en from "./en";
 import fr from "./fr";
+import { I18n } from 'i18n-js';
+import * as Localization from 'expo-localization';
 
 const i18n = new I18n({ fr, en });
 
@@ -9,7 +9,6 @@ i18n.locale = Localization.getLocales()[0].languageCode ?? 'fr';
 i18n.enableFallback = true;
 
 export default i18n;
-export const t = i18n.t.bind(i18n);
 export function defaultLocale(locale: string) {
   i18n.locale = locale
 }
