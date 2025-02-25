2625cf34e65416c500ffebd8d8cf02de8b3fac93
Gabin8K <gabindjomo21@gmail.com>
2025-02-24 23:44:07 -0500

    adding env programticaly

--- app.config.ts
+++ app.config.ts
@@ -1,15 +1,16 @@
 import { ExpoConfig, ConfigContext } from 'expo/config';
+import { env } from './env';
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  name: config.name || 'Elocate CMR',
+  name: config.name || 'Elocate',
   slug: config.slug || 'elocate',
   android: {
     ...(config.android || {}),
     config: {
       ...(config.android?.config || {}),
       googleMaps: {
-        apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAP_API_KEY
+        apiKey:env.GOOGLE_MAP_API_KEY
       }
     }
   },
@@ -17,14 +18,14 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     ...(config.ios || {}),
     config: {
       ...(config.ios?.config || {}),
-      googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAP_API_KEY
+      googleMapsApiKey:env.GOOGLE_MAP_API_KEY
     }
   },
   extra: {
     ...(config.extra || {}),
     env: {
       ...(config.extra?.env || {}),
-      apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAP_API_KEY,
+      apiKey:env.GOOGLE_MAP_API_KEY
     }
   }
 });
